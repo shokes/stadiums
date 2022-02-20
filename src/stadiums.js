6ac62564495530b8be81ea67e94db23f59ef59e3
Oshoke <oyatioshoke@yahoo.com>
2022-02-19 22:35:33 -0800

    update

--- src/stadiums.js
+++ src/stadiums.js
@@ -1,31 +1,40 @@
+import React, { useState } from "react";
+
 const Stadiums = function ({ stadium }) {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <>
       {stadium.map((stad) => {
-        // console.log(stad);
         const { image, id, name, description, capacity } = stad;
 
-        const removeHandler = function () {
-          console.log("button working");
-        };
+        // console.log(id);
 
         return (
           <article key={id} className="article">
             <img src={image} alt={name} />
-            <div className="flex">
-              <h2>{name}</h2>
-              <h3 className="capacity">{capacity}</h3>
-            </div>
+            <footer>
+              <div className="flex">
+                <h2>{name}</h2>
+                <h3 className="capacity">{capacity}</h3>
+              </div>
 
-            <h4>
-              {description}
-              <p>Read More</p>
-            </h4>
-            <div className="button-center">
-              <button className="btn" onClick={removeHandler}>
-                Remove This
-              </button>
-            </div>
+              <p>
+                {readMore ? description : `${description.substring(0, 200)}...`}{" "}
+                <button
+                  onClick={() => {
+                    setReadMore(!readMore);
+                  }}
+                  className="link"
+                >
+                  {readMore ? "show less" : "read more"}
+                </button>
+              </p>
+
+              <div className="button-center">
+                <button className="btn">Remove</button>
+              </div>
+            </footer>
           </article>
         );
       })}
